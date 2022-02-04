da512df0cfcfab93d5553afd5a8495a66a0a36e7
AiryTheDragon <purifyingflame@gmail.com>
2022-02-04 14:21:38 -0500

    Created Speech bubblee

--- Assets/Player/PlayerScripts/Player.cs
+++ Assets/Player/PlayerScripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
@@ -8,12 +9,16 @@ public class Player : MonoBehaviour
     [SerializeField] float walkSpeed = 2f;
     [SerializeField] float runSpeed = 4f;
     [SerializeField] float currentSpeed = 2f;
+    [SerializeField] float messageDuration = 5f;
 
+    private float messageTimeRemaining;
+    private bool isMessage = false;
 
+    public GameObject speechObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speechObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -38,6 +43,47 @@ public class Player : MonoBehaviour
 
         transform.Translate(xChange, 0, 0);
         transform.Translate(0, yChange, 0);
+
+        if (isMessage)
+        {
+            messageTimeRemaining -= Time.deltaTime;
+
+            if (messageTimeRemaining < 0)
+            {
+                speechObject.SetActive(false);
+                isMessage = false;
+            }
+        }
+
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            createMessage("Ouch.");
+        }
+
+        if (collision.gameObject.tag == "Door")
+        {
+            createMessage("I don't have the key.");
+        }
+
+    }
+
+
+    void createMessage(string text)
+    {
+        speechObject.SetActive(true);
+
+        speechObject.GetComponentInChildren<TextMeshPro>().text = text;
+        messageTimeRemaining = messageDuration;
+        isMessage = true;
+    }
+
+
+
+
 
 }
