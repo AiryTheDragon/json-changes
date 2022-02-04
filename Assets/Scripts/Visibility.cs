5d1fbff851c95c92565dde246598b7274b32c6d7
AiryTheDragon <purifyingflame@gmail.com>
2022-02-04 10:56:05 -0500

    Created a test scene, created a building and a player with movement, with speed adjusting by pressing the mouse or left alt or ctrl keys

---
+++ Assets/Scripts/Visibility.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visibility : MonoBehaviour
+{
+    [SerializeField] Color32 showObject = new Color32(255, 255, 255, 255);
+    [SerializeField] Color32 noShowObject = new Color32(255, 255, 255, 128);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Ceiling")
+        {
+            SpriteRenderer spriteRenderer = collider.GetComponentInChildren<SpriteRenderer>();
+            spriteRenderer.color = noShowObject;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Ceiling")
+        {
+            SpriteRenderer spriteRenderer = collider.GetComponentInChildren<SpriteRenderer>();
+            spriteRenderer.color = showObject;
+        }
+
+    }
+
+}
