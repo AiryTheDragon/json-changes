ac9535b00ad7f6a66a25b7184367d9c1d7394893
Daniel Masker <DMasker@macleanfogg.com>
2022-03-07 16:12:29 -0600

    Added group of activities clocktime comparison

--- Assets/NPC/Activities/GroupOfActivities.cs
+++ Assets/NPC/Activities/GroupOfActivities.cs
@@ -8,8 +8,65 @@ public class GroupOfActivities : MonoBehaviour
 
     public List<Activity> Activities;
 
-    public ClockTime startTime;
+    // TODO: Make this a clocktime.
+    //public ClockTime startTime;
+    public int StartHour;
+    public int StartMinute;
+    public int EndHour;
+    public int EndMinute;
+    public bool LoopMidnight;
 
+    public bool TimeWithin(ClockTime time)
+    {
+        if(!LoopMidnight)
+        {
+            if(time.Hour > StartHour)
+            {
+                if ((time.Hour < EndHour) || (time.Hour == EndHour && time.Minute < EndMinute))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (time.Hour == StartHour)
+            {
+                if(time.Minute >= StartMinute)
+                {
+                    if ((time.Hour < EndHour) || (time.Hour == EndHour && time.Minute < EndMinute))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if((time.Hour > StartHour || (time.Hour == StartHour && time.Minute >= StartMinute)) ||
+                ((time.Hour < EndHour) || (time.Hour == EndHour && time.Minute < EndMinute))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
