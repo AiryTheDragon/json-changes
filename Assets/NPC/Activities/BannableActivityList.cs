3829e98d22ee8d72ba7f5ecb330e8d779710a95a
AiryTheDragon <purifyingflame@gmail.com>
2022-05-06 06:10:07 -0400

    Updated programming code to handle banning and unbanning activities

---
+++ Assets/NPC/Activities/BannableActivityList.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BannableActivityList : MonoBehaviour
+{
+    public List<ActivityCategory> ActivityCategories;
+
+}
--- Assets/NPC/BannedActivitiesBehavior.cs
+++ Assets/NPC/BannedActivitiesBehavior.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BannedActivitiesBehavior : MonoBehaviour
+public class BannedActivitiesBehavior : MonoBehaviour, INeedsClockUpdate
 {
     public List<Activity> BannedActivities;
 
     public List<int> BanLevels;
+
+    public BannableActivityList ListOfBannableActivities;
+
+    private ClockBehavior Clock;
 
     public void BanActivity(Activity activity)
     {
@@ -37,7 +41,8 @@ public class BannedActivitiesBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Clock = GameObject.Find("Clock").GetComponent<ClockBehavior>();
+        Clock.NeedsClockUpdate.Add(this);
     }
 
     // Update is called once per frame
@@ -45,4 +50,128 @@ public class BannedActivitiesBehavior : MonoBehaviour
     {
         
     }
+
+    public void UpdateClock(ClockTime time)
+    {
+        // time to ban and unban activities (every hour)
+        if (time.Minute==0)
+        {
+            UpdateBans();
+
+        }
+    }
+    private void UpdateBans()
+    {
+        // algorithm to ban and unban activities.  Choose one sleep to ban and one to unban
+        int BanValue1 = -1;
+        int BanValue2 = -1;
+
+        int UnBanValue = -1;
+        ActivityCategory ActivityBanned1 = null;
+        ActivityCategory ActivityBanned2 = null;
+        ActivityCategory ActivityUnBanned = null;
+
+        int maxValue = ListOfBannableActivities.ActivityCategories.Count;
+
+        // choose unban
+        UnBanValue = Random.Range(0, maxValue);
+        ActivityUnBanned = ListOfBannableActivities.ActivityCategories[UnBanValue];
+
+        // choose ban1
+        BanValue1 = Random.Range(0, maxValue);
+        ActivityBanned1 = ListOfBannableActivities.ActivityCategories[BanValue1];
+
+        // choose ban2
+        BanValue2 = Random.Range(0, maxValue);
+        ActivityBanned2 = ListOfBannableActivities.ActivityCategories[BanValue2];
+            
+        // if unban == ban, no unban
+        if (UnBanValue == BanValue1 || UnBanValue == BanValue2 )
+        {
+            UnBanValue = -1;
+        }
+
+        ban(ActivityBanned1);
+        ban(ActivityBanned2);
+
+        if (UnBanValue >= 0)
+        {
+            unban(ActivityUnBanned);
+        }
+
+    }
+
+    // Gets the position of an activity in the Banned activity list
+    private int getPos(Activity activity)
+    {
+        return BannedActivities.IndexOf(activity);
+    }
+
+    //unbans activities in the given ActivityCategory
+    private void unban(ActivityCategory AC)
+    {
+        bool changed = false;
+        Activity act;
+        int index = -1;
+
+        // check each activity if banned
+        for (int i = 0; i < AC.Activities.Count; i++)
+        {
+            // check if activity banned
+            act = AC.Activities[i];
+            if (BannedActivities.Contains(act))
+            {
+                index = getPos(act);
+                changed = true;
+                BannedActivities.RemoveAt(index);
+                BanLevels.RemoveAt(index);
+                Debug.Log(act + "is no longer banned");
+            }
+        }
+        if (changed)
+        {
+            Debug.Log(AC.UnBanText + " " + AC.GroupName + " is no longer banned.");
+        }
+        else
+        {
+            Debug.Log("The 'Boss' has itchy fingers.");
+        }
+    }
+
+    //bans activities in the given ActivityCategory
+    private void ban(ActivityCategory AC)
+    {
+        bool changed = false;
+        Activity act;
+        int index = -1;
+
+        // check each activity if unbanned
+        for (int i = 0; i < AC.Activities.Count; i++)
+        {
+            // check if activity unbanned
+            act = AC.Activities[i];
+            if (BannedActivities.Contains(act))
+            {
+                index = getPos(act);
+                BanLevels[index] = BanLevels[index] + 1;
+                Debug.Log(act + " is now banned at level " + BanLevels[index]);
+            }
+            else
+            {
+                changed = true;
+                BannedActivities.Add(act);
+                BanLevels.Add(1);
+                Debug.Log(act + " is now banned at level 1");
+            }
+        }
+        if (changed) // new addition to list
+        {
+            Debug.Log(AC.BanText + " " + AC.GroupName + " is now banned.");
+        }
+        else // already on list
+        {
+            Debug.Log(AC.MoreBanText + " " + AC.GroupName + " is now more strongly banned.");
+        }
+    }
+
 }
