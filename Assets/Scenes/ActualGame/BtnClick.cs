5fa6e6afcb7002328cbabe5bf995205972f0d91c
Daniel Masker <dtmasker@mtu.edu>
2022-08-26 18:54:30 -0400

    Added initial save.

--- Assets/Scenes/ActualGame/BtnClick.cs
+++ Assets/Scenes/ActualGame/BtnClick.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,6 +10,20 @@ public class BtnClick : MonoBehaviour
 {
     public void BtnNewScene()
     {
+
+        try
+        {
+            string saveLocation = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EEData/saves");
+            Directory.CreateDirectory(saveLocation);
+            string filePath = Path.Combine(saveLocation, $"save{Player.SaveFile}.json");
+            using var fileStream = File.Create(filePath);
+            using StreamWriter fileWriter = new(fileStream);
+            fileWriter.Write($"{{\"PlayerName\":\"{Player.Name}\",\"Time\":{{\"Hour\":5,\"Minute\":13,\"Day\":0}}");
+        }
+        catch (Exception)
+        {
+
+        }
         SceneManager.LoadScene("MainScene");
     }
 
