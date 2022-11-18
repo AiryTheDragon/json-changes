28b042e1fbaf034bc0e2a79bcedee4e179d2933c
Daniel Masker <dtmasker@mtu.edu>
2022-11-18 16:10:43 -0500

    Merged

--- Assets/Scenes/ActualGame/QuitGame.cs
+++ Assets/Scenes/ActualGame/QuitGame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Steamworks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,6 +9,13 @@ public class QuitGame : MonoBehaviour
 {
     public void BtnQuit()
     {
+        #if !(UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX || STEAMWORKS_WIN || STEAMWORKS_LIN_OSX)
+            #define DISABLESTEAMWORKS
+        #endif
+
+        #if !DISABLESTEAMWORKS
+            SteamAPI.Shutdown();
+        #endif
         Application.Quit();
     }
 
