918d6862245b0e04e2f428988d072e754c8bc827
Daniel Masker <dtmasker@mtu.edu>
2022-11-18 21:26:05 -0500

    Pushed stuff

--- Assets/Scenes/ActualGame/StartScene/NotePanelBehavior.cs
+++ Assets/Scenes/ActualGame/StartScene/NotePanelBehavior.cs
@@ -32,6 +32,33 @@ public class NotePanelBehavior : MonoBehaviour
 
                 NextButton.GetComponent<Transform>().localPosition = new Vector3(0, -438, 0);
                 NextButton.GetComponent<RectTransform>().sizeDelta = new Vector2(235, 128);
+                NextButton.GetComponentInChildren<TextMeshProUGUI>().fontSize = 67;
+                break;
+            case Resolutions.R1280X1024:
+                Background.GetComponent<Transform>().localScale = new Vector3(1, 1.1f, 1);
+                Text.GetComponent<Transform>().localPosition = new Vector3(0.8032f, 58.0265f, 0);
+                Text.GetComponent<RectTransform>().sizeDelta = new Vector2(558.29f, 766.5801f);
+                Text.GetComponent<TextMeshProUGUI>().fontSize = 36;
+
+                Paper.GetComponent<Transform>().localPosition = new Vector3(0, 58, 0);
+                Paper.GetComponent<Transform>().localScale = new Vector3(0.8345f, 0.727f, 1);
+
+                NextButton.GetComponent<Transform>().localPosition = new Vector3(0, -438, 0);
+                NextButton.GetComponent<RectTransform>().sizeDelta = new Vector2(235, 128);
+                NextButton.GetComponentInChildren<TextMeshProUGUI>().fontSize = 67;
+                break;
+            case Resolutions.R3840X2160:
+                Background.GetComponent<Transform>().localScale = new Vector3(2, 2.2f, 1);
+                Text.GetComponent<Transform>().localPosition = new Vector3(0.8032f, 58.0265f, 0);
+                Text.GetComponent<RectTransform>().sizeDelta = new Vector2(1116.58f, 1533.1602f);
+                Text.GetComponent<TextMeshProUGUI>().fontSize = 72;
+
+                Paper.GetComponent<Transform>().localPosition = new Vector3(0, 58, 0);
+                Paper.GetComponent<Transform>().localScale = new Vector3(1.669f, 1.454f, 1);
+
+                NextButton.GetComponent<Transform>().localPosition = new Vector3(0, -918, 0);
+                NextButton.GetComponent<RectTransform>().sizeDelta = new Vector2(470, 256);
+                NextButton.GetComponentInChildren<TextMeshProUGUI>().fontSize = 134;
                 break;
         }
     }
