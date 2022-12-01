aea21cd91fee5d042751873999a8735c4f37df54
Daniel Masker <dtmasker@mtu.edu>
2022-11-30 20:59:03 -0500

    Added 4K resolution for the main game. Works on #94

--- Assets/UI/MainCanvasBehavior.cs
+++ Assets/UI/MainCanvasBehavior.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainCanvasBehavior : MonoBehaviour
 {
@@ -40,12 +41,33 @@ public class MainCanvasBehavior : MonoBehaviour
                 BugReportButton.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
 
                 ClockTimeDisplay.GetComponent<RectTransform>().anchoredPosition = new Vector3(-865, 466, 0);
-                ClockTimeDisplay.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                ClockTimeDisplay.GetComponent<RectTransform>().sizeDelta = new Vector2(200, 100);
+                ClockTimeDisplay.GetComponent<Text>().fontSize = 42;
 
                 NPCInfoMenu.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
                 NPCInfoMenu.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
 
                 ConfirmationMenu.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                break;
+            case Resolutions.R3840X2160:
+                // Setup Always Visible elements.
+                Minimap.GetComponent<Transform>().localPosition = new Vector3(-1446, 645, 0);
+                Minimap.GetComponent<Transform>().localScale = new Vector3(2, 2, 1);
+                
+                SuspicionBar.GetComponent<RectTransform>().anchoredPosition = new Vector3(1634, 904, 0);
+                SuspicionBar.GetComponent<RectTransform>().localScale = new Vector3(2, 2, 1);
+
+                BugReportButton.GetComponent<RectTransform>().anchoredPosition = new Vector3(1630, 752, 0);
+                BugReportButton.GetComponent<RectTransform>().localScale = new Vector3(2, 2, 1);
+
+                ClockTimeDisplay.GetComponent<RectTransform>().anchoredPosition = new Vector3(-1752, 894, 0);
+                ClockTimeDisplay.GetComponent<RectTransform>().sizeDelta = new Vector2(400, 200);
+                ClockTimeDisplay.GetComponent<Text>().fontSize = 84;
+
+                NPCInfoMenu.GetComponent<RectTransform>().localScale = new Vector3(2, 2, 1);
+                NPCInfoMenu.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+
+                ConfirmationMenu.GetComponent<RectTransform>().localScale = new Vector3(2, 2, 1);
                 break;
         }
     }
