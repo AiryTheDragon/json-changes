58ec724c6c75107468a002cfbb395cef833ac39d
Daniel Masker <dtmasker@mtu.edu>
2022-11-30 20:45:40 -0500

    Added default resolution for the main game. Works on #94

--- Assets/UI/MainCanvasBehavior.cs
+++ Assets/UI/MainCanvasBehavior.cs
@@ -10,6 +10,46 @@ public class MainCanvasBehavior : MonoBehaviour
 
     public GameObject ConfirmationMenu;
 
+    public GameObject Minimap;
+
+    public GameObject SuspicionBar;
+
+    public GameObject BugReportButton;
+
+    public GameObject ClockTimeDisplay;
+
+    public GameObject NPCInfoMenu;
+
+
+    public void Start()
+    {
+        var settings = new GeneralSettings();
+        settings.LoadSettings();
+
+        switch(GeneralSettings.Settings.ScreenSize)
+        {
+            case Resolutions.R1920X1080:
+                // Setup Always Visible elements.
+                Minimap.GetComponent<Transform>().localPosition = new Vector3(-721, 344, 0);
+                Minimap.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+                
+                SuspicionBar.GetComponent<RectTransform>().anchoredPosition = new Vector3(817, 452, 0);
+                SuspicionBar.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+
+                BugReportButton.GetComponent<RectTransform>().anchoredPosition = new Vector3(815, 376, 0);
+                BugReportButton.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+
+                ClockTimeDisplay.GetComponent<RectTransform>().anchoredPosition = new Vector3(-865, 466, 0);
+                ClockTimeDisplay.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+
+                NPCInfoMenu.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                NPCInfoMenu.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+
+                ConfirmationMenu.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                break;
+        }
+    }
+
     /// <summary>
     /// Returns true if any menu that could cover a clickable is open.
     /// </summary>
