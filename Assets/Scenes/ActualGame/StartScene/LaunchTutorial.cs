30e083ccba6f318c9700f79748c076275af04209
AiryTheDragon <purifyingflame@gmail.com>
2023-01-09 22:53:13 -0500

    Created tutorialscene code, attached to tutorial button, fixed gardening ban text.

---
+++ Assets/Scenes/ActualGame/StartScene/LaunchTutorial.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchTutorial : MonoBehaviour
+{
+    public void TutorialScene()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+
+
+}
